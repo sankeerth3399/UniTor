502ec7681c21bbd1cabb8e131082e3876ce74181
Sankeerth <133137769+sankeerth3399@users.noreply.github.com>
2025-12-14 17:30:13 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,7 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
-import { defineConfig, loadEnv } from 'vite'
-import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
@@ -24,14 +21,3 @@ export default defineConfig(({ mode }) => {
       }
     };
 });
-  const env = loadEnv(mode, process.cwd(), '');
-  return {
-    plugins: [react()],
-    // REPLACE 'repo-name' WITH YOUR GITHUB REPOSITORY NAME
-    base: '/repo-name/', 
-    define: {
-      // This allows your existing code using process.env.API_KEY to work
-      'process.env.API_KEY': JSON.stringify(env.API_KEY), 
-    },
-  }
-})
